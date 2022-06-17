dab3e04aa04d9adcabcedb6e660fe33c9b7eee0b
Danny Lemmers <51800738+DannyLemmers@users.noreply.github.com>
2022-06-17 13:11:25 +0200

    Added means

--- epochedEMG.m
+++ epochedEMG.m
@@ -1,11 +1,19 @@
-function epochedData = epochedEMG(data, presamples, aftersamples)
+function [epochedData, means] = epochedEMG(data, presamples, aftersamples)
     indexes = find(data(:,9)==1);
+    means = [];
     for i = 1 : length(indexes)
         samples = [indexes(i)-presamples:indexes(i)+aftersamples];
-        if max(samples)>12000
+        if max(samples)>120000
+            continue
+        elseif samples(1) < 0
+            epochedData(i,:,:) = nan([length(samples),9]);
+            continue
+        elseif mean(data(samples,4),3) == 0
+            disp('mean was zero!')
             continue
         else
-            epochedData(i,:,:) = data(samples,:);
-            epochedData(i,:,1) = linspace(-presamples/200*1000,aftersamples/200*1000,aftersamples+presamples+1);
+                means =mean(data(samples,4),1);
+                epochedData(i,:,:) = data(samples,:);
+                epochedData(i,:,1) = linspace(-presamples/2000*1000,aftersamples/2000*1000,aftersamples+presamples+1);
         end
     end
